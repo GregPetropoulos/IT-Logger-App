4ae1c7b109b89d436dadab335fa549fced8728f1
GP <gregpetropoulos@yahoo.com>
2021-12-27 07:40:54 -0500

    Added info to Readme and set up log routes and confirmed that a new tech registered and login can only see their own logs through private routes.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # IT Logger
+
+## Routes
+-Protected from each tech. Each Tech can CRUD a log in their own private view. Once Ready a tech can add log to public interface
+-A tech can not look at another techs logs
+-Only logs shown on public will be seen by others
+-Routes that are protected are by design of middleware
+
+## MongoDB (NoSQL)
+The database has two collections:
+
+1.logs
+
+2.teches
+
+The collections are created by the mongoose schema in the models folder. The log schema is associated to the tech schema by objectId.
+```
+const mongoose = require('mongoose');
+
+const LogSchema = mongoose.Schema({
+  // *Specific to a users logs
+    tech:{
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'techs' 
+    },
+  ```
+ This allows the tech to have their own private view of the logs on their account. 
+
+## Validation
+The express-validator npm package was utilized to handle the backend validation for `Registration`, `Login`, and `Log routes`.
+The validation comes after the auth middleware,
+``` 
+check('firstName','A first name is required').not.isEmpty(), (req,res)....
+```
+`.not` negates the result of the next validator in the chain and the `.isEmpty()` returns a boolean value for no errors within the result object
+https://express-validator.github.io/docs/validation-result-api.html#isempty
+
+## Authentication
+A tech will have an authenticated route to login into after being registered. A middleware is used at login to check and verify the JSON Web Token. The tech.id is then stored in the payload to be used as reference of the logged in user. 
+### JSON Web Token (JWT)
+The JSON Web Token is used to identify the payload data
+https://jwt.io/
+
--- routes/logs.js
+++ routes/logs.js
@@ -6,33 +6,61 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
 // use to protect routes
-const auth = require ('../middleware/auth');
+const auth = require('../middleware/auth');
 
 // *BRING IN THE DB FOR LOGS
 const Log = require('../models/Log');
-
 
 // * @route     GET api/logs
 // * @desc      Get all the logs
 // * access     public
 router.get('/', auth, async (req, res) => {
-try {
-  // *!console.log('check this value', req.tech.id)
-  // In the Log schema a tech field is the objectId, the auth middleware gives access to the req.tech object in payload of decoded....Essentially find the _id in the database that matches the token in header 
-  const logs = await Log.find({tech:req.tech.id}).sort({date:-1});
-  res.json({logs});
-} catch (err) {
-  console.error(err.message);
-  req.status(500).send('Server Error');
-}
+  try {
+    // *!console.log('check this value', req.tech.id)
+    // In the Log schema a tech field is the objectId, the auth middleware gives access to the req.tech object in payload of decoded....Essentially find the _id in the database that matches the token in header
+    const logs = await Log.find({ tech: req.tech.id }).sort({ date: -1 });
+    res.json(logs);
+  } catch (err) {
+    console.error(err.message);
+    req.status(500).send('Server Error');
+  }
 });
 
 // * @route     POST api/logs
 // * @desc      Add a new log
-// * access     public
-router.post('/', async (req, res) => {
-  res.send(' Post for adding a new log');
-});
+//! @access   Private
+
+router.post(
+  '/',
+  auth,
+  check('message', 'A message is required').not().isEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    //*!if the errors show up from validationResult send 400 array
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //*Set up a new Log
+    const { message, attention, date } = req.body;
+    try {
+      const newLog = new Log({
+        tech: req.tech.id,
+        message,
+        attention,
+        date
+      });
+
+      // Save to db
+      const log = await newLog.save();
+      // Return log to client
+      res.json(log);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // * @route     PUT api/logs/:id
 // * @desc      Edit a log
@@ -45,6 +73,6 @@ router.put('/:id', (req, res) => {
 // * @desc      Delete a log
 // * access     public
 router.delete('/:id', (req, res) => {
-    res.send(' Delete the log');
-  });
+  res.send(' Delete the log');
+});
 module.exports = router;
