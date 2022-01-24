b2f1b8158e1d9da5204f466906cac29b3c8c99bf
GP <gregpetropoulos@yahoo.com>
2022-01-24 07:17:22 -0500

    StatusCard fucntionality completed minor UI changes needed, used local state with useState hooks to manage the state until further enhancements to add the servers status to redux

--- client/src/components/layout/StatusCards.js
+++ client/src/components/layout/StatusCards.js
@@ -1,68 +1,103 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const StatusCards = () => {
-  // FAKE DB
-  const server = [
-    { name: 'Server 1', id: 1, status: true, counter: 0 },
-    { name: 'Server 2', id: 2, status: true, counter: 0 },
-    { name: 'Server 3', id: 3, status: true, counter: 0 }
-  ];
+  // * STATE of 3 Servers
+  const [serverState, setServerState] = useState({
+    servers: [
+      {
+        switcher: true,
+        name: 'Server 1',
+        sid: 1,
+        counter: 0,
+        date: ''
+      },
+      {
+        switcher: true,
+        name: 'Server 2',
+        sid: 2,
+        counter: 0,
+        date: ''
+      },
+      {
+        switcher: true,
+        name: 'Server 3',
+        sid: 3,
+        counter: 0,
+        date: ''
+      }
+    ]
+  });
 
-  // STATE of 3 false values to start
-  const [checked, setChecked] = useState(new Array(server.length).fill(false));
-  //    const [serverStatus, setServerStatus]=useState(true)
-  //    need to set checkbox up
-  //  need to set up back end db, editmodal,  and redux for server
-
-  const onChange = (position) => {
-    const checked = setChecked.map((item, key) =>
-      key === position ? !item : item
+  const { servers } = serverState;
+  const onChange = (id) => {
+    let updatedList = servers.map((item) =>
+      item.sid === id
+        ? {
+            ...item,
+            switcher: !item.switcher,
+            date: new Date(),
+            counter: item.counter + 1
+          }
+        : item
     );
+
+    setServerState({ ...serverState, servers: updatedList });
   };
+  console.log(serverState.servers);
   return (
-    <div className=' row valign-wrapper'>
-      {server.map((arr, index) => (
-        <div key={arr.id} className='col s12 m5'>
-          <div className=' z-depth-4 hoverable grey darken-4 center card'>
-            <i
-              className={
-                checked
-                  ? 'light-green-text text-accent-3 center-align large material-icons'
-                  : 'red-text center-align large material-icons'
-              }>
-              change_history
-            </i>
-            <p
-              className={
-                checked ? 'light-green-text text-accent-3 ' : 'red-text'
-              }>
-              {arr.name}
-            </p>
-            <p
-              className={
-                checked ? 'light-green-text text-accent-3 ' : 'red-text'
-              }>
-              {checked ? <span>UP</span> : <span>DOWN</span>}
-            </p>
-            <div className='switch center'>
-              <label>
-                UP
-                <input
-                  type='checkbox'
-                  id={`arr.id-${index}`}
-                  name={arr.name}
-                  value={arr.name}
-                  checked={checked[index]}
-                  onChange={(e) => setChecked(index)}
-                />
-                <span className='lever'></span>
-                DOWN
-              </label>
+    <Fragment>
+      <div className=' row valign-wrapper'>
+        <div className='col s12 m5'>
+          {serverState.servers.map((arr) => (
+            <div
+              key={arr.sid}
+              className=' z-depth-4 hoverable grey darken-4 center card'>
+              <i
+                className={
+                  arr.switcher
+                    ? 'light-green-text text-accent-3 center-align large material-icons'
+                    : 'red-text center-align large material-icons'
+                }>
+                change_history
+              </i>
+              <p
+                className={
+                  arr.switcher ? 'light-green-text text-accent-3 ' : 'red-text'
+                }>
+                {arr.name}
+              </p>
+              <p
+                className={
+                  arr.switcher ? 'light-green-text text-accent-3 ' : 'red-text'
+                }>
+                {arr.switcher ? (
+                  <span>
+                    UP EVENT {arr.counter !== 0 ? 1 + arr.counter / 2 : 1}
+                  </span>
+                ) : (
+                  <span>
+                    DOWN EVENT {arr.counter && -Math.round(arr.counter / 2)}
+                  </span>
+                )}
+              </p>
+              <div className='switch center'>
+                <label>
+                  UP
+                  <input
+                    type='checkbox'
+                    checked={arr.switcher}
+                    name={arr.name}
+                    onChange={() => onChange(arr.sid)}
+                  />
+                  <span className='lever'></span>
+                  DOWN
+                </label>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </Fragment>
   );
 };
 
