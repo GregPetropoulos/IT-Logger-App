345909187e7d5b6ab989c05ab39f17ff86e1287b
GP <gregpetropoulos@yahoo.com>
2022-01-30 15:08:45 -0500

    Changed the fab couldn't get it to disable the hover.

--- client/src/components/layout/AddBtn.js
+++ client/src/components/layout/AddBtn.js
@@ -3,12 +3,14 @@ import React from 'react';
 const AddBtn = () => {
   return (
     <div className='fixed-action-btn'>
-      <a
-        href='#add-log-modal'
-        className='btn-floating btn-large blue darken-2 modal-trigger'>
-        <i className='large material-icons'>post_add</i>
+        <a
+        
+        href='#open-log-modal'
+        className='btn-floating btn-large purple darken-2 modal-trigger'>
+        <i className='large material-icons'>dashboard</i>
       </a>
       <ul>
+       
         <li>
           <a
             href='#edit-log-modal'
@@ -21,6 +23,13 @@ const AddBtn = () => {
             href='#delete-log-modal'
             className='btn-floating red modal-trigger'>
             <i className='material-icons'>delete_forever</i>
+          </a>
+        </li>
+        <li>
+          <a
+            href='#add-log-modal'
+            className='click-to-toggle btn-floating btn-large blue darken-2 modal-trigger'>
+            <i className='large material-icons'>post_add</i>
           </a>
         </li>
       </ul>
